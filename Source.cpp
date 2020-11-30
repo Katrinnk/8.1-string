acc4da5f8781b0dceeb37dfdaade6711b36a36bc
Katrinnk <katerin69llobster@gmail.com>
2020-11-30 21:49:13 +0200

    8.1

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#define TESTING
+
+using namespace std;
+
+string Change(string& str)
+{
+    if (str.length() < 2)
+    {
+        cout << "¬вед≥ть б≥льше символ≥в";
+        return str;
+    }
+
+    while (str.find("BASIC") != -1)
+    {
+        str = str.replace(str.find("BASIC"), 6, "Delphi");
+
+    }
+    return str;
+}
+#ifdef TESTING
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+    cout << "Modified string (param) : " << str << endl;
+
+    Change(str);
+
+    cout << "Modified string (result): " << str << endl;
+    return 0;
+}
+#endif
